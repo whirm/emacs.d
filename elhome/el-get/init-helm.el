3083437237d5ea40511b4ea2a8bca455314d28d8
Elric Milon <whirm@gmx.com>
2015-01-28 17:33:11 +0100

    split helm settings into its own file

---
+++ elhome/el-get/init-helm.el
@@ -0,0 +1,74 @@
+(require 'helm-config)
+;; TODO: check why this makes helm not find any results when grepping
+;; (setq
+;;  helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
+;;  helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f")
+(setq
+ helm-adaptive-history-file "~/.emacs.d/var/helm-adaptive-history"
+ helm-bookmark-show-location t
+ helm-boring-file-regexp-list (quote ("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "~$" "\\.pyo$" "\\.pyc$" "\\.elc$" "^#.*#$"))
+ helm-buffer-max-length 40
+ helm-candidate-separator "┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅"
+ helm-ff-skip-boring-files t
+ helm-ff-tramp-not-fancy nil
+ helm-findutils-ignore-boring-files t
+ helm-for-files-preferred-list (quote (helm-source-buffers-list helm-source-recentf helm-source-bookmarks helm-source-file-cache helm-source-files-in-current-dir))
+ helm-imenu-delimiter " → "
+ helm-locate-command "locate %s -r %s"
+ helm-mode t
+ helm-quick-update t
+ helm-split-window-default-side (quote same)
+ helm-yas-display-key-on-candidate t
+ )
+
+(helm-mode 1)
+(helm-adaptative-mode 1)
+
+;; Use helm
+(global-set-key (kbd "C-M-p") 'helm-semantic-or-imenu)
+(global-unset-key (kbd "C-q"))
+(global-set-key (kbd "C-q") 'helm-mini)
+(global-unset-key (kbd "M-C-q"))
+(global-set-key (kbd "M-C-q") 'helm-for-files)
+
+(global-unset-key (kbd "M-x"))
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+(global-unset-key (kbd "M-y"))
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
+(define-key global-map [remap find-file] 'helm-find-files)
+(define-key global-map [remap occur] 'helm-occur)
+(define-key global-map [remap list-buffers] 'helm-buffers-list)
+(define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
+(define-key emacs-lisp-mode-map       [remap completion-at-point] 'helm-lisp-completion-at-point)
+
+(defun helm-projectile-grep ()
+  "like projectile-grep but with helm :D"
+  (interactive)
+  (helm-do-grep-1 (list (projectile-project-root)) t)
+  )
+
+(defun helm-mini-devel ()
+  "helm-mini for prog-modes (TODO entries, helm-projectile, buffers and recentf)"
+  (interactive)
+  (require 'helm-files)
+  (require 'helm-misc)
+  (require 'helm-projectile)
+  (let ((helm-ff-transformer-show-only-basename nil))
+    (helm-other-buffer '(helm-source-fixme
+                         helm-source-projectile-buffers-list
+                         helm-source-projectile-recentf-list
+                         helm-source-projectile-files-list
+                         helm-source-buffers-list
+                         helm-source-recentf
+                         helm-source-buffer-not-found)
+                       "*helm mini devel*")))
+
+(add-hook 'projectile-mode-hook (lambda ()
+                                  (local-set-key (kbd "C-q") 'helm-mini-devel)
+                                  (local-set-key (kbd "C-c g") 'helm-projectile-grep)
+                                  (local-set-key (kbd "C-S-M-p") 'helm-etags-select)
+                                  ))
+
+(defalias 'grep 'helm-do-grep)
--- elhome/settings/settings.el
+++ elhome/settings/settings.el
@@ -78,7 +78,6 @@
  '(grep-find-ignored-files
    (quote
     (".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "de_aqui_cap_avall_son_meves" "TAGS" "#*#")))
- '(helm-yas-display-key-on-candidate t)
  '(history-delete-duplicates t)
  '(history-length 300)
  '(hl-paren-colors
