286d18ee95cbbdf99914e29c3675089b4b90f5c3
Elric Milon <whirm@gmx.com>
2015-04-30 17:19:52 +0200

    I use visual-regexp-steroids
    It's the sh*t! :D

--- elhome/startup/my-el-get.el
+++ elhome/startup/my-el-get.el
@@ -65,6 +65,17 @@
                          (add-hook 'emacs-startup-hook (lambda ()
                                                          (undo-tree-mode t)
                                                          ))))
+         (:name visual-regexp-steroids
+                :after (progn
+                         (require 'visual-regexp-steroids)
+                         (define-key global-map (kbd "C-c r") 'vr/replace)
+                         (define-key global-map (kbd "C-c q") 'vr/query-replace)
+                         ;; if you use multiple-cursors, this is for you:
+                         (define-key global-map (kbd "C-c m") 'vr/mc-mark)
+                         ;; to use visual-regexp-steroids's isearch instead of the built-in regexp isearch, also include the following lines:
+                         (define-key esc-map (kbd "C-r") 'vr/isearch-backward) ;; C-M-r
+                         (define-key esc-map (kbd "C-s") 'vr/isearch-forward) ;; C-M-s
+                         ))
          (:name bbdb)
          (:name org-mode
                 ;; :checkout "release_8.2.10"
