12cd2b95557dd4613571410e680d3af5d5932772
Elric Milon <whirm@gmx.com>
2015-07-18 00:41:01 +0200

    Smart backspace for isearch

--- elhome/startup/init.el
+++ elhome/startup/init.el
@@ -341,6 +341,27 @@
                   (join-line -1)))
 ;;^
 
+;; From https://gist.github.com/johnmastro/508fb22a2b4e1ce754e0
+(defun wh-isearch-delete-something ()
+  "Delete non-matching text or the last character."
+  ;; Mostly copied from `isearch-del-char' and Drew's answer on the page above
+  (interactive)
+  (if (= 0 (length isearch-string))
+      (ding)
+    (setq isearch-string
+          (substring isearch-string
+                     0
+                     (or (isearch-fail-pos) (1- (length isearch-string)))))
+    (setq isearch-message
+          (mapconcat #'isearch-text-char-description isearch-string "")))
+  (if isearch-other-end (goto-char isearch-other-end))
+  (isearch-search)
+  (isearch-push-state)
+  (isearch-update))
+
+(define-key isearch-mode-map (kbd "<backspace>") #'wh-isearch-delete-something)
+;;^
+
 ;; Keep region when undoing in region
 (defadvice undo-tree-undo (around keep-region activate)
   (if (use-region-p)
