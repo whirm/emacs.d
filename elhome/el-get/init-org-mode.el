f9f43f492d6007b0bd00fc84bd3c1dbc7f693708
Elric Milon <whirm@gmx.com>
2015-10-28 14:56:55 +0100

    Enhanced verify-refile-target
    Which allows you to ignore subtrees tagged with :NRT: (no refile target)

--- elhome/el-get/init-org-mode.el
+++ elhome/el-get/init-org-mode.el
@@ -241,12 +241,14 @@
 (setq org-indirect-buffer-display 'current-window)
 
 ;;;; Refile settings
-;; Exclude DONE state tasks from refile targets
-(defun bh/verify-refile-target ()
+;; Exclude DONE state tasks from refile targets, only refile to TODO/NEXT targets.
+(defun wh/verify-refile-target ()
   "Exclude todo keywords with a done state from refile targets"
-  (not (member (nth 2 (org-heading-components)) org-done-keywords)))
 
-(setq org-refile-target-verify-function 'bh/verify-refile-target)
+  (and (not (member "NRT" (org-get-tags-at)))
+       (not (member (nth 2 (org-heading-components)) org-done-keywords))))
+
+(setq org-refile-target-verify-function 'wh/verify-refile-target)
 
 ;; Do not dim blocked tasks
 (setq org-agenda-dim-blocked-tasks nil)
