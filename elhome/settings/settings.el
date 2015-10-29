efa712a1dacc5425702d0cbc32bd1a65075d35e7
Elric Milon <whirm@gmx.com>
2015-10-29 10:29:24 +0100

    more haskell settings

--- elhome/settings/settings.el
+++ elhome/settings/settings.el
@@ -63,6 +63,7 @@
      (awk-mode . "awk")
      (other . "gnu"))))
  '(column-number-mode t)
+ '(company-ghc-show-info t)
  '(compilation-always-kill t)
  '(compilation-scroll-output t)
  '(cua-rectangle-mark-key [C-M-return])
@@ -96,6 +97,7 @@
  '(grep-find-ignored-files
    (quote
     (".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "de_aqui_cap_avall_son_meves" "TAGS" "#*#")))
+ '(haskell-tags-on-save t)
  '(history-delete-duplicates t)
  '(history-length 300)
  '(horizontal-scroll-bar-mode nil)
