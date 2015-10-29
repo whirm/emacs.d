6f085f07aa7c65e536c9cdc8f0f8268de71b65d1
Elric Milon <whirm@gmx.com>
2015-10-29 09:16:16 +0100

    Delete broken comented code

--- elhome/startup/init.el
+++ elhome/startup/init.el
@@ -3,23 +3,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Pre-init stuff
 
-;;; timestamps in *Messages* THIS MAKES FLYCHECK (AND MAYBE OTHERS) STOP SHOWING STUFF IN THE MINBUFFER
-;; (defun current-time-microseconds ()
-;;   (let* ((nowtime (current-time))
-;;          (now-ms (nth 2 nowtime)))
-;;     (concat (format-time-string "[%Y-%m-%dT%T" nowtime) (format ".%d] " now-ms))))
-
-;; (defadvice message (before test-symbol activate)
-;;   (if (not (string-equal (ad-get-arg 0) "%s%s"))
-;;       (let ((deactivate-mark nil))
-;;         (save-excursion
-;;           (set-buffer "*Messages*")
-;;           (goto-char (point-max))
-;;           (if (not (bolp))
-;;               (newline))
-;;           (insert (current-time-microseconds))))))
-
-
 ;; (toggle-debug-on-quit)
 ;; (toggle-debug-on-error)
 
