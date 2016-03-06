d461465a8f6e4b7cf6c433282a9ef7f698f3f918
Elric Milon <whirm@gmx.com>
2016-03-06 21:30:26 +0100

    don't hardcode the font size on the config file
    I use different sizes set through .Xresources on different machines
    depending on the DPIs.

--- elhome/settings/settings.el
+++ elhome/settings/settings.el
@@ -248,7 +248,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#191717" :foreground "#D2DEC4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :family "Terminus"))))
+ '(default ((t (:inherit nil :stipple nil :background "#191717" :foreground "#D2DEC4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal :family "Terminus"))))
  '(ahs-face ((t (:background "gray15" :underline "burlywood4"))))
  '(ahs-plugin-whole-buffer-face ((t (:background "gray24"))))
  '(column-marker-1 ((t (:background "#250000"))))
