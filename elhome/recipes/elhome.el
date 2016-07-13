bc60a2e0e7192c9e890a6c7c1d604bb1f1b73834
Elric Milon <whirm@gmx.com>
2016-07-13 09:49:45 +0200

    add elhome recipe

---
+++ elhome/recipes/elhome.el
@@ -0,0 +1,8 @@
+(:name elhome
+       :website "https://github.com/dabrahams/elhome#readme"
+       :description "A scalable Emacs configuration framework"
+       :type github
+       :pkgname "whirm/elhome"
+       :depends (byte-code-cache)
+       :features elhome
+       :compile nil)
